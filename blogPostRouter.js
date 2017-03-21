9f52f5fe9b9a68b232d21acf34c52bb7491607f1
Peter Szujewski <pszujewski@gmail.com>
2017-03-21 15:33:16 -0400

    need to refine our endpoints and check models file

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {blogPosts} = require('./models');
 
 const router = express.Router();
 
---
+++ models.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Define Schema
+const blogSchema = new Schema({
+   // _id: { type: Schema.ObjectId, auto: true },
+    title: {type: String, unique: true, required: true},
+    author: {
+        firstName: {type: String, unique: true, required: true},
+        lastName: {type: String, unique: true, required: true}
+    },
+    content: {type: String, required: true}
+});
+
+// Define virtual properties and methods
+blogSchema.virtual('fullName').get(function() {
+    return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogSchema.methods.apiRepr = function() {
+    return {
+        title: this.title,
+        content: this.content,
+        author: this.fullName,
+        created: Date.now()
+    };
+}
+// Create our model
+const blogPosts = mongoose.model('posts', blogSchema);
+
+// export our model
+module.exports = {blogPosts};
